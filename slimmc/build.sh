f8d75b6df7bf416cf3f4537942a5190ac92f87d7
Szczepan Bednarz <plgsbed@login01.prometheus.cyfronet.pl>
2020-10-10 16:03:19 +0200

    end lines corrections

--- slimmc/build.sh
+++ slimmc/build.sh
@@ -3,15 +3,16 @@ GHASH=`git rev-parse HEAD`
 BUILD=`date`
 SYS=`uname`
 NIM=`nim --version | head -n 1`
+
 echo '>>> Building slimmc'
 PRG='slimmc'
 EXTRA=''
 printf "var prg=\"$PRG\"\nvar extra=\"$EXTRA\"\nvar gtag=\"$GTAG\"\nvar ghash=\"$GHASH\"\nvar build=\"$BUILD\"\nvar sys=\"$SYS\"\nvar nimv=\"$NIM\"\n" > version.nim
-nim c -d:release slimmc.nim
-rm version.nim
+nim c -d:release -o:slimmc11 slimmc.nim
+
 echo ">>> Building slimmc-turbo"
 PRG='slimmc-turbo'
 EXTRA='*** Warning! Memory greedy version ***'
 printf "var prg=\"$PRG\"\nvar extra=\"$EXTRA\"\nvar gtag=\"$GTAG\"\nvar ghash=\"$GHASH\"\nvar build=\"$BUILD\"\nvar sys=\"$SYS\"\nvar nimv=\"$NIM\"\n" > version.nim
-nim c -d:release --gc:none -o:slimmc-turbo slimmc.nim
+nim c -d:release --gc:none -o:slimmc-turbo11 slimmc.nim
 rm version.nim
