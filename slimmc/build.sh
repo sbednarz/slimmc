d95d6a0935e721ba8d3b719961e8b784487d4f7c
Szczepan Bednarz <sbednarz@pk.edu.pl>
2020-10-10 15:26:21 +0200

    Update build.sh

--- slimmc/build.sh
+++ slimmc/build.sh
@@ -3,18 +3,15 @@ GHASH=`git rev-parse HEAD`
 BUILD=`date`
 SYS=`uname`
 NIM=`nim --version | head -n 1`
-
 echo '>>> Building slimmc'
 PRG='slimmc'
 EXTRA=''
 printf "var prg=\"$PRG\"\nvar extra=\"$EXTRA\"\nvar gtag=\"$GTAG\"\nvar ghash=\"$GHASH\"\nvar build=\"$BUILD\"\nvar sys=\"$SYS\"\nvar nimv=\"$NIM\"\n" > version.nim
 nim c -d:release slimmc.nim
 rm version.nim
-
 echo ">>> Building slimmc-turbo"
 PRG='slimmc-turbo'
 EXTRA='*** Warning! Memory greedy version ***'
 printf "var prg=\"$PRG\"\nvar extra=\"$EXTRA\"\nvar gtag=\"$GTAG\"\nvar ghash=\"$GHASH\"\nvar build=\"$BUILD\"\nvar sys=\"$SYS\"\nvar nimv=\"$NIM\"\n" > version.nim
 nim c -d:release --gc:none -o:slimmc-turbo slimmc.nim
 rm version.nim
-
