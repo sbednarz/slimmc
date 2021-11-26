b8344edfeb64660d4a7e0ad7c318d0fe1781db1f
Szczepan Bednarz <sbednarz@pk.edu.pl>
2021-11-26 20:14:54 +0100

    Update model.nim

--- slimmc/model.nim
+++ slimmc/model.nim
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import nregex, strutils, tables, sets, algorithm
+import regex, strutils, tables, sets, algorithm
 import strformat
 
 
